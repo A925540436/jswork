49271a7c8bf8f65d2dc4f5ec91baf9ccc47df925
A925540436 <925540436@qq.com>
2019-10-14 15:40:00 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,4 +1,14 @@
 //9*9的空表，请自行编写这段代码
+str = '<table border="1">'
+for(let i=9;i>0;--i){    //外循环控制行
+    str += '<tr>'
+    for(var j=9;j>0;--j){  //内循环控制列
+        str +='<td>&nbsp;</td>'
+    }
+    str += '</tr>'
+}
+str += '</table>'
+document.getElementById('table1').innerHTML = str
 
 
 //倒九九乘法空表
@@ -14,3 +24,13 @@ str += '</table>'
 document.getElementById('table2').innerHTML = str
 
 //倒九九乘法表 ， 请自行编写这段代码
+str = '<table border="1">'
+for(var i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str +='<td>&nbsp;'+i+'x'+j+'='+(i*j) +'</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML = str
