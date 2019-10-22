a4fbe4c13e07f5bd25b7fb1224774380083f85c0
A925540436 <925540436@Qq.com>
2019-10-22 15:26:40 +0800

    work7

--- work7/utils.js
+++ work7/utils.js
@@ -1,28 +1,49 @@
-//冒泡排序
 function bubbleSort(str) {
-    //把字符串切分成数组
     let arr = str.split(',')
-    //比较轮数，数组有多少个数字，久比较数组长度-1轮
-    for (let i = 0; i < arr.length - 1; i++){
-        //从第一个开始比较相邻的两个数值
-        for (let j = 0;j < arr.length - i - 1; j++){
-            //比较相邻的两个元素，如果左面的大，那么就把大的交换到右边
-            if (arr[j] > arr[j + 1]) {
-                //使用结构赋值的方法，把两个元素交换位置
-                [arr[j], arr[j + 1]] = [arr[j + 1],arr[j]]
+    let sortLog = []
+    for (let i=0;i<arr.length-1;i++){
+        for (let j=0;j<arr.length-i-1;j++){
+            sortLog.push([arr.concat(),[j,j+1]])
+            sortLog.push([arr.concat(),[j,j+1]])
+            sortLog.push([arr.concat(),[j,j+1]])
+            if(arr[j]>arr[j+1]){
+                [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
+                sortLog.push([arr.concat(),[j,j+1]])
             }
         }
     }
-    return arr
-}//插入排序
+    sortLog.push([arr.concat(),[-1,-1]])
+    return sortLog
+}
+
 function insertSort(str) {
-    let arr = str.split(',')
-    for (let i = 1; i < arr.length; i++) {
-        for (let j = i; j > 0; j--) {
-            if (arr[j - 1] > arr[j]) {
-                [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]]
+    let arr=str.split(',')
+    let sortLog = []
+    for (let i=1;i<arr.length;i++){
+        for (let j=i;j>0;j--){
+            sortLog.push([arr.concat(),[j-1,j]])
+            sortLog.push([arr.concat(),[j-1,j]])
+            sortLog.push([arr.concat(),[j-1,j]])
+
+            if(arr[j-1]>arr[j]){
+                [arr[j-1],arr[j]]=[arr[j],arr[j-1]]
+                sortLog.push([arr.concat(),[j-1,j]])
             }
         }
     }
-    return arr
+    sortLog.push([arr.concat(),[-1,-1]])
+    return sortLog
+}
+function showLog(logValues,showElement) {
+    let str = ''
+    let {done,value:[row,pos]}=logValues.next()
+    for(const key in row){
+        let color = pos.indexOf(Number(key))>-1?'color:red;':''
+        str += '<span style="font-size: '+row[key]*20+'px;'+ color+
+            '">'+ row[key] +'</span>'
+    }
+
+    showElement.innerHTML=str
+    if(pos[0]!==-1)
+        setTimeout("showLog(logValues,showDiv)",500)
 }
